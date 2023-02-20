567ce5a95119b6dd701c52433828dc2792db13fb
Dan Sazonov <p.294803@gmail.com>
2023-02-20 09:20:18 +0300

    Замокал отправку ответов

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-// Email.send({
-//   SecureToken : "fa5cdb1d-dae7-4227-9afe-4568c372839c",
-//   To : 'p-294803@yandex.ru',
-//   From : "p.294803@gmail.com",
-//   Subject : "This is the subject",
-//   Body : "fuck"
-// }).then(message => console.log(message));
-
 let allQuestions = [
   {
     question: "q1",
@@ -209,7 +201,13 @@ function showResults() {
 }
 
 function sendResults() {
-
+// Email.send({
+//   SecureToken : "fa5cdb1d-dae7-4227-9afe-4568c372839c",
+//   To : 'p-294803@yandex.ru',
+//   From : "p.294803@gmail.com",
+//   Subject : "This is the subject",
+//   Body : "fuck"
+// }).then(message => console.log(message));
 }
 
 nextBtn.addEventListener("click", checkAns, false);

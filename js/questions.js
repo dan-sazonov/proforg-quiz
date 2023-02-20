f027514b1a31d8fe1001c3aa90f979ba427422fe
Dan Sazonov <p.294803@gmail.com>
2023-02-20 07:57:35 +0300

    Сделал форму списком

---
+++ js/questions.js
@@ -0,0 +1,31 @@
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+
+
+`
+<li class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="v10_1">
+  <label class="form-check-label" for="v10_1">
+    Вариант ответа 1
+  </label>
+</li>
+          <li class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="v10_2">
+          <label class="form-check-label" for="v10_2">
+            Вариант ответа 2
+          </label>
+        </li>
+          <li class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="v10_3">
+          <label class="form-check-label" for="v10_3">
+            Вариант ответа 3
+          </label>
+        </li>
+          <li class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="v10_4">
+          <label class="form-check-label" for="v10_4">
+            Вариант ответа 4
+          </label>
+        </li>`
